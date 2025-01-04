159c427dd594628cbbdefb798e204e2b9fc20e9f
TriforceHero_ <rowanboeger@gmail.com>
2025-01-03 23:39:44 -0800

    brimstone

--- src/main/java/com/triforcehero/chudcraft/ChudCraft.java
+++ src/main/java/com/triforcehero/chudcraft/ChudCraft.java
@@ -1,6 +1,8 @@
 package com.triforcehero.chudcraft;
 
 import com.mojang.logging.LogUtils;
+import com.triforcehero.chudcraft.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +31,7 @@ public class ChudCraft {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
 
+        ModItems.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -46,9 +49,10 @@ public class ChudCraft {
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BRIMSTONE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
